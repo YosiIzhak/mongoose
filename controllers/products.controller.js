3c12400d2676aec05816cd4a332cc29f2f1296bf
Yosi Izhak <yosii84@gmail.com>
2021-04-21 15:54:13 +0300

    first commit

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -3,11 +3,11 @@ const productModel = require('../models/products.model');
 
 const createProduct = (req, res) => {
     // const data = req.body;
-    const {id, productName, productCategory, isActive, details} = req.body;
-   console.log(req.body)
+    const { id, productName, productCategory, isActive, details } = req.body;
+    console.log(req.body)
     if (productName.length < 0) {
-        
-        return res.json({"error": "eroor"})
+
+        return res.json({ "error": "eroor" })
     }
     const product = new productModel({
         id: id,
@@ -17,33 +17,89 @@ const createProduct = (req, res) => {
         details: details
     });
     product.save((err) => {
-        if (err) return res.json({"error": err})
-        return res.json({"success": product})
+        if (err) return res.json({ "error": err })
+        return res.json({ "success": product })
     });
 
 
 }
-const getByPrice = (req, res) => {
-    productModel.find({"details.price":{$gte:50, $lte:100}}).then((products) => {
+const getByPrice = async (req, res) => {
+    try {
+        const products = await productModel.find({ "details.price": { $gte: 50, $lte: 100 } })
         return res.send(products)
-    });
+    }
+    catch (e) {
+        return res.status(200).send(e)
+    }
+    // productModel.find({"details.price":{$gte:50, $lte:100}}).then((products) => {
+    //     return res.send(products)
+    // });
 }
-const getByActive = (req, res) => {
-    const {id} = req.params;
-    productModel.find({isActive:true}).then((products) => {
+const getByActive = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const products = await productModel.find({ isActive: true })
         return res.send(products)
-    });
+    }
+    catch (e) {
+        return res.status(200).send(e)
+    }
+    // productModel.find({isActive:true}).then((products) => {
+    //     return res.send(products)
+    // });
 }
-const findById = (req, res) => {
-    const {id} = req.params;
-    productModel.findOne({id:id}).then((product) => {
+const findById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await productModel.findOne({ id: id })
         return res.send(product)
-    });
+    }
+    catch (e) {
+        return res.status(200).send(e)
+    }
+    // productModel.findOne({id:id}).then((product) => {
+    //     return res.send(product)
+    // });
 }
-const getProducts = (req, res) => {
-    productModel.find({}).then((products) => {
+const updateById = async (req, res) => {
+    const { id } = req.params;
+    const {isActive} = req.body;
+    try {
+        const product = await productModel.findOneAndUpdate({id:id},{isActive:isActive }, {new: true})
+        return res.send(product)
+    }
+    catch (e) {
+        return res.status(200).send(e)
+    }
+    // productModel.findOne({id:id}).then((product) => {
+    //     return res.send(product)
+    // });
+}
+const deleteById = async (req, res) => {
+    const { id } = req.params;
+   
+    try {
+        const product = await productModel.findOneAndDelete({id:id})
+        return res.send(product)
+    }
+    catch (e) {
+        return res.status(200).send(e)
+    }
+    // productModel.findOne({id:id}).then((product) => {
+    //     return res.send(product)
+    // });
+}
+const getProducts = async (req, res) => {
+    try {
+        const products = await productModel.find({})
         return res.send(products)
-    });
+    }
+    catch (e) {
+        return res.status(200).send(e)
+    }
+    // productModel.find({}).then((products) => {
+    //     return res.send(products)
+    // });
 }
 
 module.exports = {
@@ -51,5 +107,7 @@ module.exports = {
     getAll: getProducts,
     getId: findById,
     getActive: getByActive,
-    getPrice: getByPrice
+    getPrice: getByPrice,
+    updateId: updateById,
+    deleteId: deleteById
 }
--- routes/products.route.js
+++ routes/products.route.js
@@ -8,12 +8,17 @@ router.get('/',(req,res)=>{
 }).get('/:id',(req,res)=>{
     const id = req.params.id
     productControler.getId(req,res);
+ }).patch('/:id',(req,res)=>{
+    const id = req.params.id
+    productControler.updateId(req,res);
  }).get('/:active',(req,res)=>{
     productControler.getActive(req,res);
  }).get('/:range',(req,res)=>{
     productControler.getPrice(req,res);
  }).post('/',(req,res)=>{
     productControler.create(req,res);
+}).delete('/:id',(req,res)=>{
+    productControler.deleteId(req,res);
 })
 
 module.exports = router;
